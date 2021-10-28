482f9983de011561d32619b943610a62b8b3d7b4
JavierGold <567498jav@gmail.com>
2021-10-28 14:48:01 -0500

    sign in

--- brew_crew/lib/screens/wrapper.dart
+++ brew_crew/lib/screens/wrapper.dart
@@ -6,6 +6,6 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return either the Home or Authenticate widget
-    return Home();
+    return Authenticate();
   }
 }
